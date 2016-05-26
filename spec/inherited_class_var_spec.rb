1ae42d6df282e05d81516bb4565fc9481c20f644
Steve Chung <steve.chung7@gmail.com>
2016-05-26 17:07:12 -0400

    cleanup spec

--- spec/inherited_class_var_spec.rb
+++ spec/inherited_class_var_spec.rb
@@ -34,32 +34,6 @@ describe InheritedClassVar do
         expect(Parent.inherited_hash).to eql(test1: 'test1', test2: 'test2')
         expect(Parent.inherited_hash.object_id).to eql Parent.inherited_hash.object_id
       end
-
-      context 'with cache cleared' do
-        before do
-          # NOTE deep_clear_class_cache doesn't work here...
-          [ :@_inherited_hash, :@_inherited_hash_inherited_class_cache ].each do |variable|
-            Parent.instance_variable_set(variable, nil)
-          end
-          # Parent.send(:deep_clear_class_cache, :@_inherited_hash)
-        end
-
-        it 'inherited merge' do
-          Parent.merge_inherited_hash(test1: 'test1')
-          expect(Parent.inherited_hash).to eql(test1: 'test1')
-
-          ClassWithFamily.merge_inherited_hash(test2: 'test2')
-          expect(ClassWithFamily.inherited_hash).to eql(test1: 'test1', test2: 'test2')
-        end
-
-        it 'merge options', skip: true do
-          Parent.merge_inherited_hash(test1: { foo: 'test1' })
-          expect(Parent.inherited_hash[:test1]).to eql({ foo: 'test1' })
-
-          ClassWithFamily.merge_inherited_hash(test1: { bar: 'test2' })
-          expect(ClassWithFamily.inherited_hash[:test1]).to eql({ foo: 'test1', bar: 'test2' })
-        end
-      end
     end
   end
 
