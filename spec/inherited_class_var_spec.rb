bddedb61b64a9cfa5c80749f16ea83c13c52496a
Steve Chung <steve.chung7@gmail.com>
2016-05-31 10:50:34 -0400

    rename test classes and clean tests

--- spec/inherited_class_var_spec.rb
+++ spec/inherited_class_var_spec.rb
@@ -1,18 +1,15 @@
 require 'spec_helper'
 
 class Grandparent; end
-module Child
-  extend ActiveSupport::Concern
-end
+module Mod; end
+
 class Parent < Grandparent
-  include Child
+  include Mod
   include InheritedClassVar
 
   inherited_class_hash :inherited_hash
 end
-class ClassWithFamily < Parent; end
-
-class InheritedBaseClass; end
+class Child < Parent; end
 
 describe InheritedClassVar do
   describe '::inherited_class_hash' do
@@ -36,51 +33,49 @@ describe InheritedClassVar do
 
         Parent.merge_inherited_hash(test1: 'test1')
         expect(Parent.inherited_hash).to eql(test1: 'test1')
-
         Parent.merge_inherited_hash(test2: 'test2')
         expect(Parent.inherited_hash).to eql(test1: 'test1', test2: 'test2')
 
         expect(Parent.inherited_hash.object_id).to eql Parent.inherited_hash.object_id
-        expect(ClassWithFamily.inherited_hash).to eql(test1: 'test1', test2: 'test2')
+        expect(Child.inherited_hash).to eql(test1: 'test1', test2: 'test2')
 
         expect(Parent.raw_inherited_hash).to eql(test1: 'test1', test2: 'test2')
-        expect(ClassWithFamily.raw_inherited_hash).to eql({})
+        expect(Child.raw_inherited_hash).to eql({})
       end
-    end
-  end
-
-  describe '::inherited_ancestors' do
-    it 'returns the inherited ancestors' do
-      expect(ClassWithFamily.send(:inherited_ancestors)).to eql [ClassWithFamily, Parent]
     end
   end
 
   describe '::inherited_custom_class' do
     let(:klass) { Parent }
-    subject { klass.send(:inherited_custom_class, :does_not_exist, InheritedBaseClass) }
+    let(:inherited_base_class) { Class.new { def self.name; "InheritedBaseClass" end } }
+    subject { klass.send(:inherited_custom_class, :does_not_exist, inherited_base_class) }
 
     it 'gives a name' do
       expect(subject.name).to eql 'ParentInheritedBaseClass'
     end
   end
 
+  describe '::inherited_ancestors' do
+    it 'returns the inherited ancestors' do
+      expect(Child.send(:inherited_ancestors)).to eql [Child, Parent]
+    end
+  end
+
   context 'with deep_inherited_class_var set' do
     let(:variable_name) { :@inherited_class_var }
-    def inherited_class_var; ClassWithFamily.send(:inherited_class_var, variable_name, [], :+) end
+    def inherited_class_var; Child.send(:inherited_class_var, variable_name, [], :+) end
 
     before do
-      [Grandparent, Parent, Child, ClassWithFamily].each do |klass|
-        klass.instance_variable_set(variable_name, [klass.to_s])
-      end
+      [Grandparent, Parent, Mod, Child].each { |klass| klass.instance_variable_set(variable_name, [klass.to_s]) }
     end
 
     describe "::class_var" do
       it "returns the class variable" do
-        [Parent, ClassWithFamily].each do |klass|
+        [Parent, Child].each do |klass|
           expect(klass.send(:class_var, variable_name, [])).to eql [klass.to_s]
         end
 
-        [Grandparent, Child].each do |klass|
+        [Grandparent, Mod].each do |klass|
           expect { klass.send(:class_var, variable_name, []) }.to raise_error(NoMethodError)
         end
       end
@@ -88,7 +83,7 @@ describe InheritedClassVar do
 
     describe '::inherited_class_var' do
       it 'returns a class variable merged across ancestors until #{described_class}' do
-        expect(inherited_class_var).to eql %w[ClassWithFamily Parent]
+        expect(inherited_class_var).to eql %w[Child Parent]
       end
 
       it 'caches the result' do
@@ -99,7 +94,7 @@ describe InheritedClassVar do
     describe '::clear_class_cache' do
       it 'clears the cache' do
         value = inherited_class_var
-        expect { ClassWithFamily.clear_class_cache(variable_name) }.to change {
+        expect { Child.clear_class_cache(variable_name) }.to change {
           value.object_id == inherited_class_var.object_id
         }.from(true).to(false)
       end
@@ -119,7 +114,7 @@ describe InheritedClassVar do
         }.from(true).to(false)
       end
 
-      it 'clears the cache of children class' do
+      it 'clears the cache of Child class' do
         value = inherited_class_var
         expect { subject }.to change {
           value.object_id == inherited_class_var.object_id
